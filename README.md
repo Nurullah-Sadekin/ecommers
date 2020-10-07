e514460d0aebf2a6230f7390239e8f2137d3f17b
Nurullah Sadekin <nurullahsadekin@gmail.com>
2020-10-08 02:41:54 +0600

    100 parcent woking

---
+++ README.md
@@ -0,0 +1,28 @@
+# Flutter Shopping App
+
+A Simple shopping app created using flutter and also using google Firebase API for database requests.
+
+## Basic Setup
+
+This app work's with google firebase, so you would be needed to create an account and get your API Keys a place them in the code. In the provider folder in lib, you need to add the API URL in *auth.dart*, *orders.dart*, *product.dart* and *products.dart* files in place of **API Key**.
+
+## Demo
+
+Here is my twitter post where I give the [demo of the app](https://twitter.com/MathurAditya7/status/1206662248996818945).
+
+## Build With
+
+- Flutter - The SDK
+- Dart - The programming language
+- provider - State Management
+- intl - For internationalized/localized dates
+- http - Future-based API for HTTP requests
+- shared_preferences - For reading and writing simple key-value pairs
+
+## Authors
+
+**Aditya Mathur**, you can find me on [Twitter](https://twitter.com/mathuraditya7) or [LinkedIn](https://www.linkedin.com/in/aditya-mathur-7240/)
+
+## License
+
+This project is licensed under the MIT License
---
+++ lib/providers/products.dart
@@ -0,0 +1,133 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:my_shop/models/http_exception.dart';
+
+import './product.dart';
+
+class Products with ChangeNotifier {
+  List<Product> _items = [];
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
+  }
+
+  final String authToken;
+  final String userId;
+
+  Products(this.authToken, this.userId, this._items);
+
+  List<Product> get items {
+    return [..._items];
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
+
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+    final filterString =
+        filterByUser ? '&orderBy="creatorId"&equalTo="$userId"' : '';
+    var url =
+        'https://ecommers-24070.firebaseio.com/products.json?auth=$authToken$filterString';
+    try {
+      final response = await http.get(url);
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      final List<Product> loadedProducts = [];
+      if (extractedData != null) {
+        url =
+            "https://ecommers-24070.firebaseio.com/userFavorites/$userId.json?auth=$authToken";
+        final favoriteResponse = await http.get(url);
+        final favoriteData = json.decode(favoriteResponse.body);
+        extractedData.forEach((prodId, prodData) {
+          loadedProducts.add(Product(
+            id: prodId,
+            title: prodData['title'],
+            price: prodData['price'],
+            description: prodData['description'],
+            imageUrl: prodData['imageUrl'],
+            isFavorite:
+                favoriteData == null ? false : favoriteData[prodId] ?? false,
+          ));
+        });
+      }
+      _items = loadedProducts;
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw error;
+    }
+  }
+
+  Future<void> addProduct(Product product) async {
+    final url =
+        'https://ecommers-24070.firebaseio.com/products.json?auth=$authToken';
+    try {
+      // Sending the request to the server to save the data
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'title': product.title,
+            'description': product.description,
+            'imageUrl': product.imageUrl,
+            'price': product.price,
+            'creatorId': userId,
+          },
+        ),
+      );
+      // Saving the id of the Product
+      final newProduct = Product(
+        title: product.title,
+        price: product.price,
+        description: product.description,
+        imageUrl: product.imageUrl,
+        id: json.decode(response.body)['name'],
+      );
+      // Adding the new item
+      _items.add(newProduct);
+      // Telling all the Listeners.
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw error;
+    }
+  }
+
+  Future<void> updateProduct(String id, Product newProduct) async {
+    final prodIndex = _items.indexWhere((prod) => prod.id == id);
+    if (prodIndex >= 0) {
+      final url =
+          'https://ecommers-24070.firebaseio.com/products/$id.json?auth=$authToken';
+      await http.patch(
+        url,
+        body: json.encode(
+          {
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price,
+          },
+        ),
+      );
+      _items[prodIndex] = newProduct;
+      notifyListeners();
+    }
+  }
+
+  Future<void> deleteProduct(String id) async {
+    final url =
+        'https://ecommers-24070.firebaseio.com/products/$id.json?auth=$authToken';
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+    _items.removeAt(existingProductIndex);
+    notifyListeners();
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Could no delete product.');
+    }
+    existingProduct = null;
+  }
+}
